b5aa1ff29aa37b442215787f2dde75e827d79f6b
Bird <edbird1@yahoo.com>
2015-08-19 08:55:43 -0600

    Add html js css

---
+++ javascript.js
@@ -0,0 +1,9 @@
+var currentGen = 1;
+var totalGen = 19;
+var totalMW = 0;
+while (currentGen <=4){
+  totalMW +=62;
+  console.log("Generator #" + currentGen + " is on, adding 62MW, for a total of " + totalMW + " MW!");
+  currentGen++;
+}
+
